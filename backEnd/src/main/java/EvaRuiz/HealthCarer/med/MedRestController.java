f09908a876d4f1844e9614fd78c08464e9dfd7ff
Eva <alcorcon03@gmail.com>
2024-09-10 08:55:09 +0200

    Added first aproximation to image managment

--- backEnd/src/main/java/EvaRuiz/HealthCarer/med/MedRestController.java
+++ backEnd/src/main/java/EvaRuiz/HealthCarer/med/MedRestController.java
@@ -3,7 +3,9 @@ package EvaRuiz.HealthCarer.med;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.Collection;
 import java.util.Optional;
@@ -17,6 +19,11 @@ public class MedRestController {
 
     @Autowired
     private MedService medService;
+
+    @Autowired
+    private ImgService imgService;
+
+    private static final String MEDS_FOLDER = "resources/meds";
 
     @GetMapping("/")
     public Collection<Med> getMeds() {
@@ -36,6 +43,25 @@ public class MedRestController {
                 .buildAndExpand(med.getId()).toUri();
         return ResponseEntity.created(location).body(med);
     }
+    @PostMapping("/{id}/images")
+    public ResponseEntity<Object> uploadImage(@PathVariable long id,
+                                              @RequestParam MultipartFile boxImageFile,
+                                              @RequestParam MultipartFile pillImageFile) throws IOException {
+        Optional<Med> med = medService.findById(id);
+        if (med.isPresent()) {
+            URI location = fromCurrentRequest().build().toUri();
+            Med med1 = med.get();
+            med1.setBoxImage(location.toString());
+            med1.setPillImage(location.toString());
+            medService.save(med1);
+            imgService.saveBoxImage(MEDS_FOLDER, med1.getId(), boxImageFile);
+            imgService.savePillImage(MEDS_FOLDER, med1.getId(), pillImageFile);
+            return ResponseEntity.created(location).build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Med> deleteMed(@PathVariable Long id) {
