8bbe21db5af94afb1df13352576828a99fac80c3
Eva <alcorcon03@gmail.com>
2024-09-12 08:21:12 +0200

    Added RestAssured dependencies and tried first test on addMed

--- backEnd/src/test/java/EvaRuiz/HealthCarer/HealthCarerApplicationTests.java
+++ backEnd/src/test/java/EvaRuiz/HealthCarer/HealthCarerApplicationTests.java
@@ -1,13 +1,39 @@
 package EvaRuiz.HealthCarer;
 
-import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HealthCarerApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	int port;
+	@BeforeEach
+	public void setUp() {
+		RestAssured.port = port;
 	}
 
+
+	@Test
+	public void medAddTest() {
+		given().
+				body("{\"name\": \"Ibuprofeno\", \"stock\": 5, \"instructions\": \"Tomar con agua\", \"dose\": 1}").
+				contentType("application/json").
+				when().
+				post("/api/meds/").
+				then().statusCode(201).assertThat().contentType("application/json").
+				body("name", equalTo("Ibuprofeno")).
+				body("stock", equalTo(5.0F)).
+				body("instructions", equalTo("Tomar con agua")).
+				body("dose", equalTo(1.0F));
+	}
+		;
 }
